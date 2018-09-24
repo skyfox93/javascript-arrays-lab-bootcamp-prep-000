de534f88bd2cea5f1506a9f68bc4f633760d1e35
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-24 22:05:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Add your functions and code here
 function destructivelyAppendKitten(name){kittens.push(name);}
 function destructivelyPrependKitten(name){kittens.unshift(name);}
-function destructivelyRemoveLastKitten(){kittens.pop()}
-function destructivelyRemoveFirstKitten
-function appendKitten
+function destructivelyRemoveLastKitten(){kittens.pop();}
+function destructivelyRemoveFirstKitten(){kittens.shift()}
+function appendKitten(name){return array.slice(0).push(name);}
 function prependKitten
 function removeLastKitten
 function removeFirstKitten
